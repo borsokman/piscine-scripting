5c378df3733adcece90881e4e223fc6dca086e05
Sagyn <borsokman0211@gmail.com>
2026-02-17 21:56:44 +0200

    777

--- hard-perm.sh
+++ hard-perm.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-chmod 401 0
-chmod 402 1
-chmod 604 2
-chmod 777 3
-chmod 510 4
-chmod 460 5
-chmod 460 6
-chmod 510 7
-chmod 604 8
-chmod 402 9
-chmod 401 A
+chmod 401 hard-perm/0
+chmod 402 hard-perm/1
+chmod 604 hard-perm/2
+chmod 777 hard-perm/3
+chmod 510 hard-perm/4
+chmod 460 hard-perm/5
+chmod 460 hard-perm/6
+chmod 510 hard-perm/7
+chmod 604 hard-perm/8
+chmod 402 hard-perm/9
+chmod 401 hard-perm/A
