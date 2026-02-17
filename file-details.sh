6f6b8ad33246b2891769355de55a3cf087a2091c
Sagyn <borsokman0211@gmail.com>
2026-02-17 22:34:23 +0200

    for linux

--- file-details.sh
+++ file-details.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-ls -l --time-style="+%Y-%m-%d %H:%M" hard-perm | sed 1d | awk '{print $1, $6, $7, $8}'
+# stat -f "%Sp %Sm %N" -t "%Y-%m-%d %H:%M" hard-perm/*
+
+ls -l --time-style="+%Y-%m-%d %H:%M" | sed 1d | awk '{print $1, $6, $7, $8}'
