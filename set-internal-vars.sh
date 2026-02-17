8374c29d13dd7cd1d01a0fb453683a67667c52b2
Sagyn <borsokman0211@gmail.com>
2026-02-17 23:22:17 +0200

    asd

--- set-internal-vars.sh
+++ set-internal-vars.sh
@@ -5,4 +5,4 @@ MY_NUM=100
 MY_PI=3.142
 MY_ARR=(one two three four five)
 
-printf "%s\n%s\n%s\n%s\n" "$MY_MESSAGE" "$MY_NUM" "$MY_PI" "$(IFS=', '; echo "${MY_ARR[*]}")"
+echo -e "$MY_MESSAGE\n$MY_NUM\n$MY_PI\n$(IFS=, ; echo "${MY_ARR[*]}")"
