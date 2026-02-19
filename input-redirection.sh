568bc389183d95ea666803bf674691fd6dd730fb
Sagyn <borsokman0211@gmail.com>
2026-02-19 18:30:04 +0200

    asdf

--- input-redirection.sh
+++ input-redirection.sh
@@ -3,8 +3,8 @@
 cat << heredoc > show-info.sh
 #!/bin/bash
 cat -e << EOF
-The current directory is: $PWD
-The default paths are: $PATH
-The current user is: $USER
+The current directory is: \$PWD
+The default paths are: \$PATH
+The current user is: \$USERNAME
 EOF
 heredoc
