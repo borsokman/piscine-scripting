7d00b42664f48be28551745594fc4207422112dc
Sagyn <borsokman0211@gmail.com>
2026-02-19 18:26:28 +0200

    asd

--- show-info.sh
+++ show-info.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cat -e << EOF
-The current directory is: /Users/sagynbek.osmonaliev/Gritlab/piscine-scripting
+The current directory is: /Users/sosmonal/Gritlab/piscine-scripting
 The default paths are: /opt/homebrew/opt/node@20/bin:/opt/homebrew/bin:/usr/local/bin:/System/Cryptexes/App/usr/bin:/usr/bin:/bin:/usr/sbin:/sbin:/var/run/com.apple.security.cryptexd/codex.system/bootstrap/usr/local/bin:/var/run/com.apple.security.cryptexd/codex.system/bootstrap/usr/bin:/var/run/com.apple.security.cryptexd/codex.system/bootstrap/usr/appleinternal/bin:/opt/pmk/env/global/bin:/usr/local/go/bin:/opt/homebrew/bin:/Users/sagynbek.osmonaliev/Library/Application Support/Code/User/globalStorage/github.copilot-chat/debugCommand:/Users/sagynbek.osmonaliev/Library/Application Support/Code/User/globalStorage/github.copilot-chat/copilotCli:/opt/homebrew/opt/node@20/bin:/Users/sagynbek.osmonaliev/.cargo/bin
-The current user is: sagynbek.osmonaliev
+The current user is: sosmonal
 EOF
