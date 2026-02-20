9344d22b3a27350e6c71e495c6639dd9a398406c
Sagyn <borsokman0211@gmail.com>
2026-02-20 23:35:39 +0200

    comparat

--- comparator.sh
+++ comparator.sh
@@ -3,7 +3,7 @@
 if [[ $# -ne 2 ]]; then
     echo "Error: The script only works with two arguments!"
     exit 0
-elif ! [[ $1 =~ ^[0-9]+$ ]] || ! [[ $2 =~ ^[0-9]+$ ]]; then
+elif ! [[ $1 =~ ^-?[0-9]+$ ]] || ! [[ $2 =~ ^-?[0-9]+$ ]]; then
     echo "Error: Only two numeric arguments are acceptable!"
     exit 0
 elif [[ $1 -gt $2 ]]; then
