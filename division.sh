a4148a140bae7893119f7d95de32af612ec7e604
Sagyn <borsokman0211@gmail.com>
2026-02-20 18:52:37 +0200

    div

---
+++ division.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+if [[ $# != 2 ]]; then
+    echo "Error: two numbers must be provided"
+    exit 1
+elif ! [[ $1 =~ ^[0-9]+$ ]] || ! [[ $2 =~ ^[0-9]+$ ]]; then
+    echo "Error: both arguments must be integers"
+    exit 1
+elif [[ $2 == 0 ]]; then
+    echo "Error: division by zero is not allowed"
+    exit 1
+else
+    division=$(BC_LINE_LENGTH=0 bc <<< "$1 / $2")
+    echo $division
+fi
