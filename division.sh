337ba900864a7717b1eb920faf9b359088555775
Sagyn <borsokman0211@gmail.com>
2026-02-20 18:54:24 +0200

    divi

--- division.sh
+++ division.sh
@@ -10,6 +10,6 @@ elif [[ $2 == 0 ]]; then
     echo "Error: division by zero is not allowed"
     exit 1
 else
-    division=$(BC_LINE_LENGTH=0 bc <<< "$1 / $2")
-    echo $division
+    result=$(echo "$1 / $2" | bc)
+    echo $result
 fi
