81d27b2a4c019f8ca4693533a5dadd36ab9086b2
Sagyn <borsokman0211@gmail.com>
2026-02-20 19:32:45 +0200

    divi4

--- division.sh
+++ division.sh
@@ -2,18 +2,13 @@
 
 if [[ $# -ne 2 ]]; then
     echo "Error: two numbers must be provided"
-    exit 1
 # Check if both arguments are valid integers
 elif ! [[ $1 =~ ^-?[0-9]*\.?[0-9]+$ ]] || ! [[ $2 =~ ^-?[0-9]*\.?[0-9]+$ ]]; then
     echo "Error: both arguments must be integers"
-    exit 1
 # Check for division by zero
-elif [[ $2 -eq 0 ]]; then
-    echo "Error: division by zero is not allowed"
-    exit 1
+elif [ $(echo "$2 == 0" | bc) -eq 1 ]; then
+	echo "Error: division by zero is not allowed"
 else
     result=$(echo "$1 / $2" | bc)
-
-	# Output the result
 	echo $result
 fi
