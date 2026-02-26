94353e6d1c01ffe4ad8cf302fe0f573fe88c3a91
Sagyn Gay <borsokman0211@gmail.com>
2026-02-27 00:58:55 +0200

    objtosjonn12

--- object_to_json.py
+++ object_to_json.py
@@ -1,32 +1,35 @@
 import json
 
-class User:
-    def __init__(self):
-        self.username = 'user'
-        self.email = 'something@mail.com'
-        self.is_valid = False
 
-def create_new_user(data):
-    user = User()
+class User:
+    username = 'user'
+    email = 'something@mail.com'
+
+
+def create_new_user(json_string):
     try:
-        # Parse JSON string to dict
-        if isinstance(data, str):
-            data = json.loads(data)
-        
-        if isinstance(data, dict) and "username" in data and "email" in data:
-            user.username = data["username"]
-            user.email = data["email"]
-            user.is_valid = True
-    except (json.JSONDecodeError, TypeError):
-        pass
-    
-    return user
+        data = json.loads(json_string)
+    except json.JSONDecodeError:
+        return User()
+
+    if isinstance(data, dict) and "username" in data and "email" in data:
+        user = User()
+        user.username = data["username"]
+        user.email = data["email"]
+        return user
+
+    return User()
+
 
 def user_to_json(user):
-    if hasattr(user, 'is_valid') and user.is_valid:
-        return json.dumps({
-            "username": user.username,
-            "email": user.email
-        })
-    else:
+    # Only return JSON if it's a custom user
+    if (
+        user.username == User.username
+        and user.email == User.email
+    ):
         return json.dumps({})
+
+    return json.dumps({
+        "username": user.username,
+        "email": user.email
+    })
