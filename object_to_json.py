85e070864ab9a08df1463296333e93b4c29f7058
Sagyn Gay <borsokman0211@gmail.com>
2026-02-27 00:56:50 +0200

    objtosjonn

--- object_to_json.py
+++ object_to_json.py
@@ -3,16 +3,29 @@ import json
 class User:
     username = 'user'
     email = 'something@mail.com'
+    is_valid = False
 
 def create_new_user(data):
     user = User()
-    if isinstance(data, dict) and "username" in data and "email" in data:
-        user.username = data["username"]
-        user.email = data["email"]
+    try:
+        # Parse JSON string to dict
+        if isinstance(data, str):
+            data = json.loads(data)
+        
+        if isinstance(data, dict) and "username" in data and "email" in data:
+            user.username = data["username"]
+            user.email = data["email"]
+            user.is_valid = True
+    except (json.JSONDecodeError, TypeError):
+        pass
+    
     return user
 
 def user_to_json(user):
-    return json.dumps({
-        "username": user.username,
-        "email": user.email
-    })
+    if user.is_valid:
+        return json.dumps({
+            "username": user.username,
+            "email": user.email
+        })
+    else:
+        return json.dumps({})
