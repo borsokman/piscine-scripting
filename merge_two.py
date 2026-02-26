cb325c6c9794e7588374a024261970d63344af83
Sagyn Gay <borsokman0211@gmail.com>
2026-02-27 00:21:26 +0200

    merge

---
+++ merge_two.py
@@ -0,0 +1,10 @@
+import json
+
+def merge_two(first_dict):
+    while True:
+        key = input("Add a new entry:\nkey: ")
+        if key == "exit":
+            break
+        value = int(input("value: "))
+        first_dict[key] = value
+    return first_dict
